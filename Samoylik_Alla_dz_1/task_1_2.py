577641facd7406fc57203d7f77d8a9bbdb7218c2
AllaSamoylik <zzajka@yandex.ru>
2021-10-14 21:43:19 +0300

    PEP 8 check

--- Samoylik_Alla_dz_1/task_1_2.py
+++ Samoylik_Alla_dz_1/task_1_2.py
@@ -3,28 +3,28 @@ numbers = []
 for i in range(1, 1001, 2):
     numbers.append(i ** 3)
 
-begin = 0
+first = 0
 last = 0
 numbers_by_7 = []
 
 for numb in range(len(numbers)):
-    begin = numbers[numb]
-    summ = 0
-    while begin >= 10:
-        last = begin % 10
-        summ += last
-        begin = begin // 10
+    first = numbers[numb]
+    sum_of_digits = 0
+    while first >= 10:
+        last = first % 10
+        sum_of_digits += last
+        first = first // 10
     else:
-        summ += begin
+        sum_of_digits += first
 
-    if summ % 7 == 0:
+    if sum_of_digits % 7 == 0:
         numbers_by_7.append(numbers[numb])
 
-summ_total = 0
+sum_total = 0
 
 for i in range(len(numbers_by_7)):
-    summ_total += numbers_by_7[i]
-print(summ_total)
+    sum_total += numbers_by_7[i]
+print(sum_total)
 
 # Добавляем 17
 
@@ -33,25 +33,25 @@ numbers = []
 for i in range(1, 1001, 2):
     numbers.append(i ** 3 + 17)
 
-begin = 0
+first = 0
 last = 0
 numbers_by_7 = []
 
 for numb in range(len(numbers)):
-    begin = numbers[numb]
-    summ = 0
-    while begin >= 10:
-        last = begin % 10
-        summ += last
-        begin = begin // 10
+    first = numbers[numb]
+    sum_of_digits = 0
+    while first >= 10:
+        last = first % 10
+        sum_of_digits += last
+        first = first // 10
     else:
-        summ += begin
+        sum_of_digits += first
 
-    if summ % 7 == 0:
+    if sum_of_digits % 7 == 0:
         numbers_by_7.append(numbers[numb])
 
-summ_total = 0
+sum_total = 0
 
 for i in range(len(numbers_by_7)):
-    summ_total += numbers_by_7[i]
-print(summ_total)
+    sum_total += numbers_by_7[i]
+print(sum_total)
